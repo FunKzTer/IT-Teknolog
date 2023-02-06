aceb8bba37918e107201d56ae7a25c6197bb5c53
FunKzTer <116255938+FunKzTer@users.noreply.github.com>
2023-02-06 09:45:35 +0100

    Add files via upload

---
+++ lpthw/ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Anders S. Schiller'
+my_age = 27
+my_height = 175 # Centimeter
+my_wheight = 74 # Kilogram
+my_eyes = 'Dark brown'
+my_teeth = 'White... ish...'
+my_hair = 'Blackhole black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_wheight} kilograms heavy.")
+print("Actually not that heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are {my_teeth} which could be better, accourding to his dentist,")
+
+# This line is tricky, try to get it exaclty right
+total = my_age + my_height + my_wheight
+print(f"If i add {my_age}, {my_height}, and {my_wheight}, i get a total of... {total}")
+print("which is a sum i guess?...")
